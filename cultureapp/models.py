411e9e1e08879310308a127905aef9ba97540f8b
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-01 19:20:15 +0300

    creating profile class model

--- cultureapp/models.py
+++ cultureapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.timezone import now
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact = models.IntegerField(default=0)
+    profile_pic = CloudinaryField('profile')
+    bio = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user.username
