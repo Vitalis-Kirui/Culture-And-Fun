5e150ab8c11018214bb8cfa003deadd02e5b2c8e
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-01 19:49:11 +0300

    registering models to admin

--- cultureapp/admin.py
+++ cultureapp/admin.py
@@ -1,3 +1,10 @@
+from cultureapp.models import Booking, Profile, visitplan, services
 from django.contrib import admin
 
-# Register your models here.
+class visitplanAdmin(admin.ModelAdmin):
+    filter_horizontal =('service',)
+
+admin.site.register(Profile)
+admin.site.register(Booking)
+admin.site.register(visitplan,visitplanAdmin)
+admin.site.register(services)
