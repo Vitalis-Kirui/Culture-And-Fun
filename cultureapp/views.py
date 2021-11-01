9b0c0fa132abdf9325a2900de59aa96ee4961d8a
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-01 22:59:14 +0300

    ppushing content on index template

--- cultureapp/views.py
+++ cultureapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from cultureapp.models import Booking, Profile, visitplan, services
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    plans = visitplan.objects.all().order_by('price')
+    service = services.objects.all()
 
+    return render(request, 'index.html', {'plans': plans, 'service': service})
 def about(request):
     return render(request, 'about.html')
