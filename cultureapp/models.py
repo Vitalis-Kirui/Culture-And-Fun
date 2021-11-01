bcddbdc38fc4cc44a647b8dd8e5843bd799f6c48
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-01 19:22:59 +0300

    creating service class model

--- cultureapp/models.py
+++ cultureapp/models.py
@@ -29,3 +29,10 @@ class Profile(models.Model):
 
     def update_profile(cls, id):
         Profile.objects.get(user_id=id)
+
+
+class services(models.Model):
+    service = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.service
